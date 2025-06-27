5e58b5d1ff7fbcd2ff2de85520028cf4b27d4a99
lucarl07 <junitinho2007@gmail.com>
2025-06-27 20:03:34 -0300

    ✨ feat: Update translatePriority's types + add double translation

--- src/utils/translatePriority.ts
+++ src/utils/translatePriority.ts
@@ -1,14 +1,31 @@
-import type { TaskPriority, TranslatedTaskPriority } from "utils/types";
+import type { TaskPriority, PortugueseTaskPriority } from "utils/types";
 
 export default function translatePriority(
-  priority: TaskPriority
-): (TranslatedTaskPriority | 'N/A') {
-  switch (priority) {
-    case 'Low': return 'Baixa'
-    case 'Medium': return 'Média'
-    case 'High': return 'Alta'
-    default:
-      console.error(`[TypeError] The given priority level "${priority}" does not exist.`)
+  priority: TaskPriority | PortugueseTaskPriority,
+  toLanguage: 'pt' | 'en'
+): (PortugueseTaskPriority | TaskPriority | 'N/A') {
+  
+  // ENGLISH TO PORTUGUESE
+  if (toLanguage === 'pt') {
+      switch (priority) {
+      case 'Low': return 'Baixa'
+      case 'Medium': return 'Média'
+      case 'High': return 'Alta'
+      default:
+        console.error(`[TypeError] The given priority level "${priority}" does not exist.`)
+    }
   }
+
+  // PORTUGUESE TO ENGLISH
+  if (toLanguage === 'en') {
+      switch (priority) {
+        case 'Baixa': return 'Low'
+        case 'Média': return 'Medium'
+        case 'Alta': return 'High'
+        default:
+          console.error(`[TypeError] The given priority level "${priority}" does not exist.`)
+      }
+  }
+
   return 'N/A'
 }
