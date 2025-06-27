4e92cbf6f7d14fea0c353c8d34e25676a618d801
lucarl07 <junitinho2007@gmail.com>
2025-06-27 17:07:07 -0300

    🚧 Add a placeholder Modal in Column
    "in" = UI-wise, for each Column; code-wise, inside src/components/Column.tsx.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,15 +1,14 @@
 import * as types from 'utils/types'
 import styles from 'styles/Column.module.css'
+
+import Modal from 'components/Modal'
 import TaskCard from 'components/TaskCard'
 import { useDroppable } from '@dnd-kit/core'
+import { useState } from 'react'
 
 interface ColumnProps {
   column: types.Column
   tasks: types.Task[]
-}
-
-function onAddCard(column: types.Column): void {
-  console.log(`Adicionar cartão na coluna "${column.name}"`)
 }
 
 function onCheckOptions(): void {
@@ -18,30 +17,38 @@ function onCheckOptions(): void {
 
 function Column({ column, tasks }: ColumnProps) {
   const { setNodeRef } = useDroppable({ id: column.id })
+  
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
   return (
-    <section className={styles.column}>
-      <header>
-        <h1>{column.name}</h1>
-        <div className={styles.button_wrapper}>
-          <button onClick={() => onAddCard(column)}>+</button>
-          <button onClick={() => onCheckOptions()}>…</button>
-        </div>
-      </header>
-      <hr />
-      <div 
-        ref={setNodeRef}
-        className={styles.task_list_wrapper}>  
-          <div className={styles.task_list}>
-            {tasks.map(task => (
-              <TaskCard key={task.id} task={task}/>
-            ))}
+    <>
+      <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h1>This is a placeholder modal.</h1>
+        <p>On next commits, this will be replaced by modal layouts inside the /layouts folder.</p>
+      </Modal>
+      <section className={styles.column}>
+        <header>
+          <h1>{column.name}</h1>
+          <div className={styles.button_wrapper}>
+            <button onClick={() => setIsModalOpen(true)}>+</button>
+            <button onClick={() => onCheckOptions()}>…</button>
           </div>
-          <button onClick={() => onAddCard(column)}>
-            + Adicionar cartão
-          </button>
-      </div>
-    </section>
+        </header>
+        <hr />
+        <div 
+          ref={setNodeRef}
+          className={styles.task_list_wrapper}>  
+            <div className={styles.task_list}>
+              {tasks.map(task => (
+                <TaskCard key={task.id} task={task}/>
+              ))}
+            </div>
+            <button onClick={() => setIsModalOpen(true)}>
+              + Adicionar cartão
+            </button>
+        </div>
+      </section>
+    </>
   )
 }
 
