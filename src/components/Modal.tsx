4a4a606c66dd472d0f434e5fd0eadfbdd0a90233
lucarl07 <junitinho2007@gmail.com>
2025-06-27 17:03:58 -0300

    ✨ feat: New Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,28 @@
+import { type ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+import styles from 'styles/Modal.module.css'
+
+interface ModalProps {
+  name?: string
+  open: boolean
+  children?: ReactNode
+  onClose(): void
+}
+
+export default function Modal({ name, open, children, onClose }: ModalProps) {
+  return open && createPortal(
+    <>
+      <div className={styles.overlay} onClick={onClose} />
+      <div className={styles.modal}>
+        <header className={styles.header}>
+          <h1>{name || 'Modal'}</h1>
+          <button onClick={onClose}>X</button>
+        </header>
+        <article className={styles.content}>
+          {children}
+        </article>
+      </div>
+    </>,
+    document.body
+  )
+}
