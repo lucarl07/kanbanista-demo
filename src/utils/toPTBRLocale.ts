756a2494090fa9b62bf05377941d874d0e8d86d2
lucarl07 <junitinho2007@gmail.com>
2025-06-27 20:13:37 -0300

    ✨ feat: New util toPTBRLocale

---
+++ src/utils/toPTBRLocale.ts
@@ -0,0 +1,25 @@
+
+
+export default function toPTBRLocale(value: Date, type: 'date' | 'time') {
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit'
+  }
+  
+  switch (type) {
+    case 'date':
+      return value.toLocaleDateString('pt-BR', dateOptions)
+    
+    case 'time':
+      return value.toLocaleTimeString('pt-BR', timeOptions)
+    
+    default:
+      console.error(`The given type "${type}" does not exist.`)
+      break;
+  }
+}
