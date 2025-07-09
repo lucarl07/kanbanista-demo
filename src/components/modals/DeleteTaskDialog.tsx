08e596acde75e9a2fd20d6cfffdc5d2a448eacdf
lucarl07 <junitinho2007@gmail.com>
2025-07-09 13:34:01 -0300

    💄 feat: Stylize DeleteTaskDialog

--- src/components/modals/DeleteTaskDialog.tsx
+++ src/components/modals/DeleteTaskDialog.tsx
@@ -9,24 +9,26 @@ interface Props extends SomeModalProps, TaskProps {}
 export default function DeleteTaskDialog({ task, open, onClose }: Props) {
   return (
     <Modal name="Excluir cartão" open={open} onClose={onClose}>
-      <h1 className={$.title}>
-        Tem certeza que deseja excluir o cartão?
-      </h1>
-      <ul className={$.warning_list}>
-        <li>
-          Você está prestes a excluir o cartão “{task.title}” fora da lixeira.
-        </li>
-        <li className={$.strong_warning}>
-          Essa ação é permanente e, portanto, irreversível.
-        </li>
-      </ul>
-      <div className={$.btn_group}>
-        <button>
-          Excluir
-        </button>
-        <button onClick={onClose}>
-          Cancelar
-        </button>
+      <div className={$.content_wrapper}>
+        <h1 className={$.title}>
+          Tem certeza que deseja excluir o cartão?
+        </h1>
+        <ul className={$.warning_list}>
+          <li>
+            Você está prestes a excluir o cartão “{task.title}” fora da lixeira.
+          </li>
+          <li className={$.strong_warning}>
+            Essa ação é permanente e, portanto, irreversível.
+          </li>
+        </ul>
+        <div className={$.btn_group}>
+          <button>
+            Excluir
+          </button>
+          <button onClick={onClose}>
+            Cancelar
+          </button>
+        </div>
       </div>
     </Modal>
   )
