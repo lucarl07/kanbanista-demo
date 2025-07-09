74f6b00e9d8605c0d6d1db49fa323aa7942735c4
lucarl07 <junitinho2007@gmail.com>
2025-07-08 21:08:00 -0300

    ✨ feat: New modal component DeleteTaskDialog

---
+++ src/components/modals/DeleteTaskDialog.tsx
@@ -0,0 +1,33 @@
+import $ from 'styles/DeleteTaskDialog.module.css'
+import { type Props as TaskProps } from 'components/Task'
+import Modal, { type Props as ModalProps } from 'layouts/Modal'
+
+type SomeModalProps = Pick<ModalProps, 'open' | 'onClose'>
+
+interface Props extends SomeModalProps, TaskProps {}
+
+export default function DeleteTaskDialog({ task, open, onClose }: Props) {
+  return (
+    <Modal name="Excluir cartão" open={open} onClose={onClose}>
+      <h1 className={$.title}>
+        Tem certeza que deseja excluir o cartão?
+      </h1>
+      <ul className={$.warning_list}>
+        <li>
+          Você está prestes a excluir o cartão “{task.title}” fora da lixeira.
+        </li>
+        <li className={$.strong_warning}>
+          Essa ação é permanente e, portanto, irreversível.
+        </li>
+      </ul>
+      <div className={$.btn_group}>
+        <button>
+          Excluir
+        </button>
+        <button onClick={onClose}>
+          Cancelar
+        </button>
+      </div>
+    </Modal>
+  )
+}
