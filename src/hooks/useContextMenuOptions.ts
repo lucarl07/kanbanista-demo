9354c942b1bc97a05c71e3bfa56052263e1e4eef
lucarl07 <junitinho2007@gmail.com>
2025-07-14 16:03:48 -0300

    👌 style: Remove console.log inside hook useContextMenuOptions

--- src/hooks/useContextMenuOptions.ts
+++ src/hooks/useContextMenuOptions.ts
@@ -35,7 +35,6 @@ export default function useContextMenuOptions(): Output {
         window.alert('(T.B.D)')
         break;
       case 'viewTask':
-        console.log(`Chegou até a função reducer!`)
         return { ...state, isViewTaskOpen: action[1] }
       case 'deleteTask':
         return { ...state, isDeleteTaskOpen: action[1] }
